782d252833f9cb0fed13919c50ad37b68d2a8f02
Aditi Sharma <99550333+aditi3004@users.noreply.github.com>
2022-12-02 23:45:17 +0530

    Create graphCreated.java

---
+++ graphCreated.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class cityNode{
+	int cityNo;
+	String cityName;
+	cityNode next;
+	
+	public cityNode(int cityNo,String cityName) {
+		this.cityNo=cityNo;
+		this.cityName=cityName;
+		this.next=null;
+	}
+}
+
+public class graphCreated {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
